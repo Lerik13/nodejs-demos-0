f470611b5b9686c3432d667f0cd995d0f4152f2a
Valeria <lerik13@gmail.com>
2021-06-18 11:37:32 -0600

    add Error page

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ console.log(chalk.blue(text));
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const { reset } = require('chalk')
 
 const server = http.createServer((req, res) => {
+/*
 	if (req.url === '/') {
 		//const path_file = path.join('file:///',__dirname, 'public', 'index.html');
 		//fs.readFile(path_file, (err, data) => {
@@ -24,8 +26,43 @@ const server = http.createServer((req, res) => {
 			res.end(data)
 		})
 	}
+*/
+	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+	const ext = path.extname(filePath)
+	if (!ext) {
+		filePath += '.html'
+	}
+
+	console.log(filePath);
+	
+	fs.readFile(filePath, (err, content) => {
+		if (err) {
+			fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+				if (err) {
+					// Server Error = 500
+					res.writeHead(500)
+					res.end('Error')
+				} else {
+					res.writeHead(200, {
+						'Content-Type': 'text/html'
+					})
+					res.end(data)
+				}
+			})
+		} else {
+			res.writeHead(200, {
+				'Content-Type': 'text/html'
+			})
+			res.end(content)
+		}
+	})
+	res.end()
+
 })
+
 
 server.listen(3000, () => {
 	console.log('Server has been started ...');
 })
+
+
