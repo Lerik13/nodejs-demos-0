849a72457b09f61358ad64e10b2f76d2448e92f4
Valeria <lerik13@gmail.com>
2021-06-17 23:14:29 -0600

    create custom server, using http-module

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+/*
 const chalk = require('chalk')
 const text = require('./data')
 
 console.log(chalk.blue(text));
+*/
+// Creating custom Servers
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+	if (req.url === '/') {
+		//const path_file = path.join('file:///',__dirname, 'public', 'index.html');
+		//fs.readFile(path_file, (err, data) => {
+		fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+			if (err) {
+				console.log('Error: ', err);
+				throw err
+			}
+			res.writeHead(200, {
+				'Content-Type': 'text/html'
+			})
+			res.end(data)
+		})
+	}
+})
+
+server.listen(3000, () => {
+	console.log('Server has been started ...');
+})
